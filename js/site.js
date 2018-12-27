5555330672fb6da97af949a9f641a8f071bab28a
Greg Hughes <gregsthings@gmail.com>
2018-12-26 16:13:32 -0800

    Updated css and added js libraries

---
+++ js/site.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed-content', {
+    strings: ['The best preperation for tomorrow^700', '', 'Is doing your best today^700', '', 'Lets do our best work^1500 together^2000', ],
+    smartBackspace: true,
+    startDelay: 1000,
+    backDelay: 1000,
+    typeSpeed: 25,
+    backSpeed: 20
+    // onComplete: function(self) { prettyLog('onCmplete ' + self) }
+  });
