4b89b78d1aa79f47f2858df81078bf659b262b3b
Greg Hughes <gregsthings@gmail.com>
2018-12-27 04:14:06 -0800

    Updated header

--- js/site.js
+++ js/site.js
@@ -1,5 +1,9 @@
+// Smooth Scroll
+var scroll = new SmoothScroll('a[href*="#"]');
+
+// Typed.js
 var typed = new Typed('#typed-content', {
-    strings: ['The best preperation for tomorrow^700', '', 'Is doing your best today^700', '', 'Lets do our best work^1500 together^2000', ],
+    strings: ['The best preperation for tomorrow^700', '', 'Is doing your best today^700', '', 'Let\'s do our best work^1500 together^2000', ],
     smartBackspace: true,
     startDelay: 1000,
     backDelay: 1000,
