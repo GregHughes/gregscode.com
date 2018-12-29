8eb2a3a1db815a809803a1f5b4120b97e62f4ce9
Greg Hughes <gregsthings@gmail.com>
2018-12-28 20:32:31 -0800

    Fixed header and updated summary

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@ var scroll = new SmoothScroll('a[href*="#"]');
 
 // Typed.js
 var typed = new Typed('#typed-content', {
-    strings: ['The best preperation for tomorrow^700', '', 'Is doing your best today^700', '', 'Let\'s do our best work^1500 together^2000', ],
+    strings: ['The best preperation for tomorrow^700', '', 'Is doing your best today^700', '', 'Let\'s do our best work...^1800', 'Let\'s do our best work together^2000', ],
     smartBackspace: true,
     startDelay: 1000,
     backDelay: 1000,
