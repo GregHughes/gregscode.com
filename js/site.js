3c3dd24ef2d4b27dedd47f32ae591880b9887024
Greg Hughes <gregsthings@gmail.com>
2023-05-02 09:59:25 -0700

    updated header typed script

--- js/site.js
+++ js/site.js
@@ -2,11 +2,14 @@
 var scroll = new SmoothScroll('a[href*="#"]');
 
 // Typed.js
-var typed = new Typed('#typed-content', {
-    strings: ['The best preperation for tomorrow^700', '', 'Is doing your best today^700', '', 'Let\'s do our best work...^900', 'Let\'s do our best work together.^2000', ],
-    smartBackspace: true,
-    startDelay: 1200,
-    backDelay: 1000,
-    typeSpeed: 25,
-    backSpeed: 20
+var typed = new Typed("#typed-content", {
+  strings: [
+    "Let's do our best work...^200",
+    "Let's do our best work together.",
+  ],
+  smartBackspace: true,
+  startDelay: 1200,
+  backDelay: 1000,
+  typeSpeed: 23,
+  backSpeed: 18,
 });
