01c3208e4880cffb0c47a4604cf7892135a4dd45
Greg Hughes <gregsthings@gmail.com>
2023-05-10 14:38:52 -0700

    new docs based deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  distDir: "docs",
 };
 
 module.exports = nextConfig;
